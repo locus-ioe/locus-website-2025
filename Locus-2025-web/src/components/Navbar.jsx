baef1719e29a0466a92dd18c4d4998c6620c7613
Divya <karkidivya5@gmail.com>
2024-11-29 14:17:11 +0545

    navbar added

---
+++ Locus-2025-web/src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../assets/logo.png'
+const Navbar = () => {
+  return (
+    <>
+    {/* <div> */}
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <img src={logo} alt="Locus 2025 Logo" className="logo" />
+        {/* <span className="navbar-title">LOCUS 2025</span> */}
+      </div>
+      <ul className="navbar-links">
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#about-us">About Us</a>
+        </li>
+        <li>
+          <a href="#events">Events</a>
+        </li>
+        <li>
+          <a href="#past-locus">Past Locus</a>
+        </li>
+        <li>
+          <a href="#sponsors">Sponsors</a>
+        </li>
+        <li>
+          <a href="#contact-us">Contact Us</a>
+        </li>
+      </ul>
+    </nav>
+    {/* </div> */}
+    </>
+  )
+}
+
+export default Navbar
---
+++ Locus-2025-web/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+const Home = () => {
+  return (
+
+    <div className='home'>
+      <Navbar/>
+      <div>hello world</div>
+    </div>
+  )
+}
+
+export default Home

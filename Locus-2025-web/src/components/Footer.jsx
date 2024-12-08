bd07c506923f0a3e68413fbf3caad1b599e571af
Divya <karkidivya5@gmail.com>
2024-12-08 16:23:26 +0545

    footer updated

--- Locus-2025-web/src/components/Footer.jsx
+++ Locus-2025-web/src/components/Footer.jsx
@@ -12,32 +12,43 @@ function Footer() {
         </p>
         <button className="footer-button">Join Us on Discord</button>
       </div>
+      <div class="custom-line"></div>
+
       <div className="footer-info">
         <div className="footer-location">
-          <button className="footer-back">Back</button>
-          <p className="location-title">IOE PULCHOWK</p>
-          <p>Pulchowk, Lalitpur</p>
-          <p>Nepal</p>
+          <div className='first-column'>
+          <button className="footer-back">Brochure</button>
+          <p className="location-title highlight">IOE PULCHOWK</p>
+          <p className='font-18'>Pulchowk, Lalitpur</p>
+          <p className='font-18'>Nepal</p>
+          </div>
         </div>
         <div className="footer-center">
-          <img src="locus-logo.png" alt="Locus 2025 Logo" className="footer-logo" />
-          <h3 className="center-title">LOCUS 2025</h3>
-          <p className="center-description">21st National Technological Festival</p>
-          <p className="footer-made-with">
-            Made with ❤️ by LOST
-          </p>
+        {/* <div > */}
+          <img src="src/assets/OWNED2 1.png" alt="Locus 2025 Logo" className="footer-logo" />
+          {/* <h3 className="center-title">LOCUS 2025</h3>
+          <p className="center-description">21st National Technological Festival</p> */}
+          {/* </div> */}
         </div>
         <div className="footer-contact">
-          <h4>Contact Us</h4>
-          <p>locus@ioe.edu.np</p>
-          <h4>Follow Us</h4>
+        <div className='third-column'>
+          <div className='third-content'><p className='highlight'>Contact Us</p>
+          <p className='font-18'>locus@ioe.edu.np</p></div>
+          <div className='third-content'><p className='highlight'>Follow Us</p>
           <div className="social-icons">
             <a href="#" className="icon">📸</a>
             <a href="#" className="icon">📘</a>
             <a href="#" className="icon">📱</a>
           </div>
+          </div>
+          </div>
         </div>
       </div>
+      <div class="custom-line"></div>
+
+      <div className="footer-made-with">
+            Made with ❤️ by LOST
+          </div>
       <div className="footer-copyright">
         ©Copyright 2024, Pulchowk Engineering Campus
       </div>

eb74ac63e41d0cfbd106f84df18515227eac5774
Jeevan-Neupane <jeevan.neupane003@gmail.com>
2024-12-10 07:46:15 +0545

    few changes

--- Locus-2025-web/src/App.jsx
+++ Locus-2025-web/src/App.jsx
@@ -7,54 +7,14 @@ import {
 } from "react-router-dom";
 import { routes } from "./routes";
 import "./App.css";
-import Layout from "./Layout";
-import Home from "./pages/Home";
-import AboutUs from "./pages/AboutUs";
-import Event from "./pages/Event";
-import PastLocusPage from "./pages/PastLocusPage";
-import Contact from "./pages/Contact";
-import Sponsor from "./pages/Sponsor";
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route
-      path='/'
-      element={<Layout />}
-    >
-      <Route
-        element={<Home />}
-        index
-      />
-
-      <Route
-        path='/about'
-        element={<AboutUs />}
-      />
-
-      <Route
-        path='/events'
-        element=<Event />
-      />
-
-      <Route
-        path='/past-locus'
-        element={<PastLocusPage />}
-      />
-
-      <Route
-        path='/sponsors'
-        element={<Sponsor />}
-      />
-
-      <Route
-        path='/contact'
-        element={<Contact />}
-      />
-    </Route>
-  )
-);
 
 function App() {
+  const router = createBrowserRouter(routes, {
+    future: {
+      v7_partialHydration: true,
+    },
+  });
+
   return (
     <>
       <RouterProvider router={router} />

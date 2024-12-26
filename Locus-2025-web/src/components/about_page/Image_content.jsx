dbbe9d7f6d97f8ab719ed6d6ca3e9ce6c276cad7
Jeevan-Neupane <jeevan.neupane003@gmail.com>
2024-12-26 15:59:07 +0545

    image made responsive

--- Locus-2025-web/src/components/about_page/Image_content.jsx
+++ Locus-2025-web/src/components/about_page/Image_content.jsx
@@ -1,16 +1,16 @@
 function Image_content({ image, description, order }) {
   return (
-    <div className="flex flex-col md:flex-row gap-10 items-center">
+    <div className="flex flex-col md:flex-row md:gap-10 gap-5 items-center md:items-start flex-wrap">
       {/* Image Section */}
       <div
         className={`flex ${
           order === 2 ? "order-2 md:order-2" : "order-1 md:order-1"
-        } w-[80%] sm:w-[40%]`}
+        } w-full sm:w-[80%] md:w-[40%] mx-auto`}
       >
         <img
           src={image}
           alt="content"
-          className="w-full h-[250px] sm:h-[300px] md:h-[350px] object-fit rounded-lg"
+          className="w-full h-auto max-h-[350px] object-cover rounded-lg"
         />
       </div>
 
@@ -18,9 +18,9 @@ function Image_content({ image, description, order }) {
       <div
         className={`flex ${
           order === 2 ? "order-1 md:order-1" : "order-2 md:order-2"
-        } w-[80%] sm:w-[60%]`}
+        } w-full sm:w-[80%] md:w-[50%]`}
       >
-        <p className="text-white text-sm md:text-base text-center md:text-left">
+        <p className="text-white text-sm sm:text-base md:text-lg text-center md:text-left">
           {description}
         </p>
       </div>

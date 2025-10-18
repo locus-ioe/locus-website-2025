6e1bd203ef9cb906d1c59b01fbb0939aef7291eb
saroj-2728 <080bct075.saroj@pcampus.edu.np>
2025-10-18 15:46:33 +0545

    feat: add events timeline component
    - Create Timeline, Card, and Badge components
    - Add events data with month-based styling
    - Update Event page with new timeline

--- Locus-2025-web/src/pages/Event.jsx
+++ Locus-2025-web/src/pages/Event.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
-import EventsList from '../components/EventsList'
+// import EventsList from '../components/EventsList'
 // import EventDescription from '../components/EventDescription'
+import EventsTimeline from '../components/events/Timeline'
+import { locusEvents } from '../data/events'
+
 const Event = () => {
   return (
-    <><EventsList /></>
+    <>
+      {/* <EventsList /> */}
+      <div>
+        <div className='text-center my-12 md:my-20 px-4'>
+          <h1 className="text-[#48d0ff] text-3xl md:text-5xl mb-3 md:mb-3">
+            <span className="text-white">LOCUS</span> Events
+          </h1>
+        </div>
+      </div>
+      <EventsTimeline title="Locus Events 2026" items={locusEvents} />
+    </>
     // <div><EventDescription /></div>
   )
 }
